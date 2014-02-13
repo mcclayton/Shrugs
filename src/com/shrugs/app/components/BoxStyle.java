46257f530f35a0c4b181d5edf2fbfe4884ffbfb7
William King <billy@bkingmedia.com>
2014-02-13 10:57:47 -0500

    Fixed BoxStyle to fit specs

--- src/com/shrugs/app/components/BoxStyle.java
+++ src/com/shrugs/app/components/BoxStyle.java
@@ -1,39 +1,38 @@
 package com.shrugs.app.components;
 
-import java.util.*;
-import java.util.Map.Entry;
 import java.awt.Color;
+import java.util.HashMap;
 
-public class BoxStyle{
-	private Map<String, String> colors;
-	public BoxStyle(){
-		colors = new HashMap<String,String>();
-		colors.put("blue", "color:rgb(0,0,255)");
-		colors.put("red", "color:rgb(255,0,0)");
-		colors.put("green", "color:rgb(0,255,0)");
-		colors.put("black", "color:rgb(0,0,0)");
-		colors.put("white", "color:rgb(255,255,255)");
-		colors.put("yellow", "color:rgb(255,255,0)");
-		colors.put("gray", "color:rgb(192,192,192)");
-		colors.put("magenta", "color:rgb(255,0,255)");
-		colors.put("cyan", "color:rgb(0,255,255)");
+public class BoxStyle implements Comparable<BoxStyle> {
+	private HashMap<String, String> style;
+	
+	public BoxStyle() {
+		style = new HashMap<String, String>();
 	}
 	
-	public String setBoxColorPreset(String color) throws ColorNotFoundException{
-		if(!colors.get(color).isEmpty()){
-			return colors.get(color);
-		}
-		else{
-			throw new ColorNotFoundException("Color is not in the presets!");
-		}
+	public String attr(String name) {
+		return style.get(name);
 	}
 	
-	public String setBoxColorViaHex(int r, int g, int b){
-		Color inputColor = new Color(r,g,b);
-		return inputColor.toString();
+	public void attr(String name, String value) {
+		style.put(name, value);
 	}
 	
-	public Set<Entry<String, String>> getColorList(){
-		return colors.entrySet();
+	public String color() {
+		return style.get("color");
 	}
+	
+	public void color(Color c) {
+		style.put("color", "rgb("+c.getRed()+","+c.getGreen()+","+c.getBlue()+")");
+	}
+	
+	public void color(int r, int g, int b) {
+		style.put("color", "rgb("+r+","+g+","+b+")");
+	}
+
+	@Override
+	public int compareTo(BoxStyle o) {
+		return (this.style.equals(o.style)) ? 1 : 0;
+	}
+	
 }
