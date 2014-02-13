d261d332dc9664910ed4dcde30e1a7898490f74d
Vipul Nataraj <vnataraj@outlook.com>
2014-02-13 11:13:30 -0500

    Added functionality for Export.java

--- src/com/shrugs/app/components/BoxStyle.java
+++ src/com/shrugs/app/components/BoxStyle.java
@@ -4,9 +4,13 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.awt.Color;
 
-public class BoxStyle{
+//some stuff is unsafe, should add exceptions when necessary
+
+public class BoxStyle implements Comparable<BoxStyle>{
 	private Map<String, String> colors;
+	private Map<String, String> attr;
 	public BoxStyle(){
+		attr = new HashMap<String,String>();
 		colors = new HashMap<String,String>();
 		colors.put("blue", "color:rgb(0,0,255)");
 		colors.put("red", "color:rgb(255,0,0)");
@@ -28,12 +32,38 @@ public class BoxStyle{
 		}
 	}
 	
-	public String setBoxColorViaHex(int r, int g, int b){
-		Color inputColor = new Color(r,g,b);
-		return inputColor.toString();
+	public void setAttribute(String name, String value){
+		attr.put(name, value);
+	}
+	
+	public String getAttribute(String name) throws AttributeNotFoundException{
+		String c = attr.get(name);
+		if(c.isEmpty()){
+			throw new AttributeNotFoundException("Attribute not found!");
+		}
+		else{
+			return c;
+		}
+	}
+	
+	public void setBoxColor(Color c){
+		this.attr.put("color", "rgb("+c.getRed()+","+c.getGreen()+","+c.getBlue()+")");
+		return;
+	}
+	
+	public void setBoxColorViaHex(int r, int g, int b){
+		this.attr.put("color", "rgb("+r+","+g+","+b+")");
 	}
 	
 	public Set<Entry<String, String>> getColorList(){
 		return colors.entrySet();
+	}
+	public String getColor(){
+		return this.attr.get("color");
+	}
+
+	@Override
+	public int compareTo(BoxStyle arg0) {
+		return (this.attr.equals(arg0)) ? 1 : 0;
 	}
 }
