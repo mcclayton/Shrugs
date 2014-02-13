a60dafaf25069e23fa9e358880f9cf7b43151508
Michael Clayton <mostly_mike@hotmail.com>
2014-02-13 14:05:09 -0500

    Added listeners to menu items.

--- src/com/shrugs/app/gui/MainMenuBar.java
+++ src/com/shrugs/app/gui/MainMenuBar.java
@@ -1,18 +1,21 @@
 package com.shrugs.app.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
-public class MainMenuBar extends JMenuBar {
+public class MainMenuBar extends JMenuBar implements ActionListener {
 	private static final long serialVersionUID = 1L;
 
+	private JMenu fileMenu, editMenu;
+	private JMenuItem openMenuItem, saveMenuItem, exportMenuItem;
+	
 	public MainMenuBar()
 	{
-		JMenu fileMenu, editMenu;
-		JMenuItem openMenuItem, saveMenuItem, exportMenuItem;
-
-
 		//Build the file menu.
 		fileMenu = new JMenu("File");
 		this.add(fileMenu);
@@ -29,6 +32,28 @@ public class MainMenuBar extends JMenuBar {
 		//Build the edit menu.
 		editMenu = new JMenu("Edit");
 		this.add(editMenu);
+		
+		//Add action listeners to menu items
+		openMenuItem.addActionListener(this);
+		saveMenuItem.addActionListener(this);
+		exportMenuItem.addActionListener(this);
+	}
+	
+	//Handle menu item clicks
+	public void actionPerformed(ActionEvent e)
+	{
+		Object source = e.getSource();
+
+		if (source instanceof JMenuItem){  //checks to see if a menu item was pressed
+			JMenuItem itemClicked = (JMenuItem) source;
+			if (itemClicked==openMenuItem){
+				JOptionPane.showMessageDialog(null, "Open Menu Item Pressed.");
+			} else if (itemClicked==saveMenuItem){
+				JOptionPane.showMessageDialog(null, "Save Menu Item Pressed.");
+			} else if (itemClicked==exportMenuItem){
+				JOptionPane.showMessageDialog(null, "Export Menu Item Pressed.");
+			}
+		}
 	}
 
 }
