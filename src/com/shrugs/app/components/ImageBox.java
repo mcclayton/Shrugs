49e157b025d22919b31de33c808f76c0a692caea
William King <billy@bkingmedia.com>
2014-03-03 16:47:54 -0500

    Added Image exporting / Image management system Added Apache Commons IO library for image copying

--- src/com/shrugs/app/components/ImageBox.java
+++ src/com/shrugs/app/components/ImageBox.java
@@ -1,31 +1,46 @@
 package com.shrugs.app.components;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 import com.google.gson.JsonObject;
-import com.google.gson.annotations.Expose;
 
 public class ImageBox extends Box {
 
-	@Expose
+	public static HashMap<String, Image> ImageList = null;
 	private Image boxImage = null;
 	private String boxImageFilePath = null;
 
-	public ImageBox(String imageFilePath, int startX, int startY, int endX, int endY) {
+	public ImageBox(String imageFilePath, int startX, int startY, int endX,
+			int endY) {
 		super(startX, startY, endX, endY);
-		this.boxImageFilePath = imageFilePath;
-		try {
-			this.boxImage = ImageIO.read(new File(imageFilePath));
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		if (ImageList == null)
+			ImageList = new HashMap<String, Image>();
+
+		this.boxImageFilePath = imageFilePath.replace('\\', '/'); //make Windows paths consistent
+		
+		if (ImageList.containsKey(boxImageFilePath))
+			this.boxImage = ImageList.get(boxImageFilePath);
+		else {
+			try {
+				this.boxImage = ImageIO.read(new File(boxImageFilePath));
+				ImageList.put(boxImageFilePath, boxImage);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+
 		this.tagName = "img";
 	}
-	
+
 	public ImageBox() {
 		super();
 		this.boxImageFilePath = null;
@@ -33,25 +48,29 @@ public class ImageBox extends Box {
 	}
 
 	public String toString() {
-		return "<img src=\""+boxImageFilePath+"\" style=\"position:absolute;left:"+getXOffset()+"px;top:"+getYOffset()+"px;width:"+width()+"px;height:"+height()+"px;"+style+"\">";
+		String[] imagearr = boxImageFilePath.split("/");
+		String name = imagearr[imagearr.length - 1];
+		return "<img src=\"img/" + name + "\" style=\"position:absolute;left:"
+				+ getXOffset() + "px;top:" + getYOffset() + "px;width:"
+				+ width() + "px;height:" + height() + "px;" + style + "\">";
 	}
 
 	public Image getImage() {
 		return this.boxImage;
 	}
-	
+
 	public String getImageFilePath() {
 		return this.boxImageFilePath;
 	}
-	
+
 	@Override
 	public JsonObject toJsonObj() {
 		JsonObject obj = super.toJsonObj();
-		obj.addProperty("img", boxImageFilePath); //TODO: get image name
-		
+		obj.addProperty("img", boxImageFilePath);
+
 		return obj;
 	}
-	
+
 	@Override
 	public void fromJsonObj(JsonObject obj) {
 		super.fromJsonObj(obj);
@@ -59,7 +78,7 @@ public class ImageBox extends Box {
 		try {
 			this.boxImage = ImageIO.read(new File(boxImageFilePath));
 		} catch (IOException e) {
-			System.err.println("Image " + boxImageFilePath +" not found.");
+			System.err.println("Image " + boxImageFilePath + " not found.");
 			e.printStackTrace();
 		}
 	}
