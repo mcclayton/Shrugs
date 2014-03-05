a197acc023b0e27d2d0a807b1dcbb0b35aa03042
William King <billy@bkingmedia.com>
2014-03-04 22:48:58 -0500

    Fixed error during export if ImageBox.ImageList hasn't been initialized

--- src/com/shrugs/app/Export.java
+++ src/com/shrugs/app/Export.java
@@ -12,49 +12,33 @@ import com.shrugs.app.components.ImageBox;
 import com.shrugs.app.gui.DrawableView;
 import org.apache.commons.io.FileUtils;
 
-
-
 public class Export {
-	
-	/* public static void main(String[] args) {
-		try {
-			BodyBox body = new BodyBox(0,0,900,1000);
-			DivBox box1 = new DivBox(10,10,300,300);
-			DivBox box2 = new DivBox(10,10,100,100);
-			DivBox box3 = new DivBox(20,20,90,90);
-			DivBox box4 = new DivBox(110,20,200,110);
-			body.addChild(box1);
-			box1.addChild(box2);
-			box2.addChild(box3);
-			box1.addChild(box4);
-			export(body);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	} */
-	
+
 	private static BufferedWriter writer;
-	
+
 	public static void export(Box box) throws IOException {
 		export(box, "output");
 	}
-	
-	//TODO: tags should be generated as <[tag] id=[id]>[content/children]</[tag]>
+
 	public static void export(Box box, String outputDir) throws IOException {
 		File path = new File(outputDir);
 		path.mkdir();
-		File indexFile = new File(outputDir+"/index.html");
+		File indexFile = new File(outputDir + "/index.html");
 		indexFile.createNewFile();
 		writer = new BufferedWriter(new FileWriter(indexFile));
 		writer.append(DrawableView.bodyBox.toString());
 		writer.close();
-		for(String imgpath : ImageBox.ImageList.keySet()) {
-			String[] patharr = imgpath.split("/");
-			String name = patharr[patharr.length-1];
-			System.out.println("Saving file: " + outputDir+"/img/"+name);
-			FileUtils.copyFile(new File(imgpath), new File(outputDir+"/img/"+name));
-		}
-		JOptionPane.showMessageDialog(null, "Export to:\n'"+outputDir+"'\nSuccessful.");
+		if (ImageBox.ImageList != null)
+			for (String imgpath : ImageBox.ImageList.keySet()) {
+				String[] patharr = imgpath.split("/");
+				String name = patharr[patharr.length - 1];
+				System.out
+						.println("Saving file: " + outputDir + "/img/" + name);
+				FileUtils.copyFile(new File(imgpath), new File(outputDir
+						+ "/img/" + name));
+			}
+		JOptionPane.showMessageDialog(null, "Export to:\n'" + outputDir
+				+ "'\nSuccessful.");
 	}
 
 }
