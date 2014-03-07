8baac6744d4143ab8658720d0d7b13c7ddf26632
Michael Clayton <mostly_mike@hotmail.com>
2014-03-07 12:19:41 -0500

    DivBoxes now export with absolute coordinates instead of relative to their parents.

--- src/com/shrugs/app/components/DivBox.java
+++ src/com/shrugs/app/components/DivBox.java
@@ -124,7 +124,7 @@ public class DivBox extends Box {
 	}
 	
 	public String toString() {
-		String str = "<div style=\"position:absolute;left:"+getXOffset()+"px;top:"+getYOffset()+"px;width:"+(height()+1)+"px;height:"+(width()+1)+"px;"+style+"\">";
+		String str = "<div style=\"position:absolute;left:"+this.startX+"px;top:"+this.startY+"px;width:"+(height()+1)+"px;height:"+(width()+1)+"px;"+style+"\">";
 		for(Box child : children)
 			str+=child;
 		str+="</div>";
